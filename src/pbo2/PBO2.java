56f5b691ce3d6fd33596bdc925d213f31de6e7ef
ramadhan <d_el12@yahoo.com>
2017-11-04 13:06:33 +0800

    Mengisi attribute dari object "m"

--- src/pbo2/PBO2.java
+++ src/pbo2/PBO2.java
@@ -2,5 +2,9 @@ package pbo2;
 public class PBO2 {
     public static void main(String[] args) {
         myFrame m = new myFrame();
+        
+        m.pesan = "Halo...";
+        m.bilBulat = 5;
+        m.bilDesimal = 3.14;
     }  
 }
